716fa6b269ca4accf278df1c2ec069dc04ed2813
root <root@valu.sein.cl>
2024-10-27 22:13:37 -0300

    Initial commit for rcsein plugin

---
+++ config.inc.php
@@ -0,0 +1,4 @@
+<?php
+// Configuración de ejemplo para el plugin de reCAPTCHA personalizado
+$config['recaptcha_public_key'] = '6LcH8m0qAAAAALFIGDA9ps5139Vpbc_q6SfG0rFs';
+$config['recaptcha_secret_key'] = '6LcH8m0qAAAAAB6qbUs9PdKfGMLzzPn4JSiPB5kQ';
---
+++ my_recaptcha.php
@@ -0,0 +1,79 @@
+<?php
+
+class my_recaptcha extends rcube_plugin
+{
+    public function init()
+    {
+        $this->load_config();
+        $rcmail = rcmail::get_instance();
+
+        // Verifica que las claves de reCAPTCHA estén configuradas
+        if ($rcmail->config->get('recaptcha_public_key') && $rcmail->config->get('recaptcha_secret_key')) {
+            $this->add_hook('template_object_loginform', [$this, 'add_recaptcha_to_login']);
+            $this->add_hook('authenticate', [$this, 'verify_recaptcha']);
+        }
+    }
+
+public function add_recaptcha_to_login($loginform)
+{
+    $rcmail = rcmail::get_instance();
+    $public_key = $rcmail->config->get('recaptcha_public_key');
+
+    // Incluye el script de reCAPTCHA y configura el modo invisible
+    $recaptcha_html = '<script src="https://www.google.com/recaptcha/api.js" async defer></script>';
+    $recaptcha_html .= '<div class="g-recaptcha" data-sitekey="' . $public_key . '" data-size="invisible" data-callback="onSubmit"></div>';
+    $recaptcha_html .= '<script>
+        function onSubmit() {
+            document.getElementById("login-form").submit();
+        }
+        function executeRecaptcha(event) {
+            event.preventDefault();
+            grecaptcha.execute();
+        }
+        document.getElementById("login-form").onsubmit = executeRecaptcha;
+    </script>';
+
+    // Inserta el reCAPTCHA invisible en el formulario de inicio de sesión
+    $loginform['content'] .= $recaptcha_html;
+
+    return $loginform;
+}
+    public function verify_recaptcha($args)
+    {
+        $rcmail = rcmail::get_instance();
+        $secret_key = $rcmail->config->get('recaptcha_secret_key');
+        $response = filter_input(INPUT_POST, 'g-recaptcha-response');
+
+        if (!$response) {
+            $rcmail->output->show_message('Por favor, verifica que no eres un robot.', 'error');
+            $args['abort'] = true;
+            return $args;
+        }
+
+        // Verifica el token de reCAPTCHA con Google
+        $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+        $data = [
+            'secret' => $secret_key,
+            'response' => $response,
+            'remoteip' => $_SERVER['REMOTE_ADDR'],
+        ];
+
+        $options = [
+            'http' => [
+                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method' => 'POST',
+                'content' => http_build_query($data),
+            ],
+        ];
+        $context = stream_context_create($options);
+        $verify = file_get_contents($recaptcha_url, false, $context);
+        $captcha_success = json_decode($verify);
+
+        if (!$captcha_success->success) {
+            $rcmail->output->show_message('La verificación de reCAPTCHA falló. Inténtalo de nuevo.', 'error');
+            $args['abort'] = true;
+        }
+
+        return $args;
+    }
+}
